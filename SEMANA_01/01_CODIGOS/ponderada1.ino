02c7f242f1f2877059ae9cbfc57f9fab44674112
lucasbmr <124476886+lucasbmr@users.noreply.github.com>
2023-10-20 08:29:32 -0300

    feat: código da ponderada semana 1

---
+++ SEMANA_01/01_CODIGOS/ponderada1.ino
@@ -0,0 +1,17 @@
+// C++ code
+//
+
+int red = 10;
+
+void setup()
+{
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(red, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(red, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
